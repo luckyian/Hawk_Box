89428107a5ed23591014fa84fbf45733b3b22930
Ian Wren <ian.wren81@gmail.com>
2020-12-16 15:53:13 -0800

    Fixed index model and had to rewrite post.js

---
+++ models/post.js
@@ -0,0 +1,23 @@
+module.exports = function (sequelize, DataTypes) {
+    const Post = sequelize.define("Post", {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        body: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        video: {
+            type: DataTypes.STRING,
+            allowNull: true
+        }
+    });
+    return Post;
+};
