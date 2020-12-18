b9de16fca90e8f177886c346be61c50312d4ad6c
Ian Wren <ian.wren81@gmail.com>
2020-12-17 20:31:19 -0800

    Added code to allign to with rest of group

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,24 +2,32 @@ const path = require("path");
 
 // Routes
 // =============================================================
-module.exports = function(app) {
 
+const db = require("../models");
+
+// Routes
+// =============================================================
+module.exports = function (app) {
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads view.html
-  app.get("/", function(req, res) {
+  app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../views/homepage.html"));
   });
-  // blog route loads blog.html
-  app.get("/blog", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/blogpost.html"));
+
+  app.get("/blogpost", function (req, res) {
+    db.Post.findAll({}).then(function (dbPost) {
+      res.render("blogpost", dbPost);
+    });
+
+    res.render("blogpost");
   });
 
-  app.get("/newpost", function(req, res){
-    res.sendFile(path.join(__dirname, "../views/newpost.html"));
+  app.get("/newpost", function (req, res) {
+    res.render("newpost");
   });
 
-  app.get("/singlepost", function(req, res){
-    res.sendFile(path.join(__dirname, "../views/singlepost.html"))
+  app.get("/singlepost", function (req, res) {
+    res.sendFile(path.join(__dirname, "../views/singlepost.html"));
   });
 };
