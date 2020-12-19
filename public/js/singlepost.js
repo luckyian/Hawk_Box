67bf786f694f21df50146727af71435b7ceb685d
Jessica Whitman <oprah135@gmail.com>
2020-12-19 13:30:09 -0800

    Took out comments in singlepost.js

--- public/js/singlepost.js
+++ public/js/singlepost.js
@@ -1,10 +1,1 @@
-// If a video isn't submitted the video section is hidden
-
-// function getSinglePost(id) {
-//   $.get("/api/posts/" + id, function (data) {
-//     if (data) {
-//       return data;
-//     }
-//   });
-// }
-// getSinglePost();
+//singlepost JS file
