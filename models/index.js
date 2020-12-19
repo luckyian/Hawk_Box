3e26e4b90c41ecfedf73e7a9a0f37c606d473ffd
Ian Wren <ian.wren81@gmail.com>
2020-12-19 14:42:11 -0800

    Added mysql to requirements in index.js

--- models/index.js
+++ models/index.js
@@ -8,6 +8,7 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
+const mysql = require("mysql");
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
