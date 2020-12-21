0a6e6064de6acdeafa3312c1ac0ee5b67ddfc9ab
Ian Wren <ian.wren81@gmail.com>
2020-12-21 14:43:42 -0800

    Edited api routes to fix bugs in passport management.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -24,6 +24,33 @@ module.exports = function (app) {
     res.redirect('/');
   });
 
+  app.post("/api/signup", function(req, res) {
+    db.User.create({
+      username: req.body.username,
+      password: req.body.password
+    })
+      .then(function() {
+        res.redirect(307, "/api/login");
+      })
+      .catch(function(err) {
+        res.status(401).json(err);
+      });
+  });
+
+  app.get("/api/user_data", function(req, res) {
+    if (!req.user) {
+      // The user is not logged in, send back an empty object
+      res.json({});
+    } else {
+      // Otherwise send back the user's user and id
+      // Sending back a password, even a hashed password, isn't a good idea
+      res.json({
+        user: req.user.user,
+        id: req.user.id
+      });
+    }
+  });
+
   app.post("/api/newpost", function (req, res) {
     // let video = req.body.video;
     // console.log(video);
