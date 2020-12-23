34518fa25f933ba689c2ebadeca5b8dae29cf07b
Jessica Whitman <oprah135@gmail.com>
2020-12-22 18:01:13 -0800

    Updated styling of homepage

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -12,7 +12,6 @@ module.exports = function (app) {
       order: [["id", "DESC"]],
       limit: 1,
     }).then((data) => {
-      console.log(data);
       res.render("homepage", data);
     });
   });
@@ -69,7 +68,6 @@ module.exports = function (app) {
       order: [["id", "DESC"]],
       limit: 1,
     }).then((data) => {
-      console.log(data);
       res.render("homepage", data);
     });
   });
